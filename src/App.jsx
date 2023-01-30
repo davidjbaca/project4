172d420cdc0dd728ca0f400ed1b85220a91ed404
David Baca <116526025+davidjbaca@users.noreply.github.com>
2023-01-30 15:12:03 -0600

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,64 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { useState } from "react";
 
+
+import SignUpPage from "./pages/SignupPage/SignupPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 
-function App() {
+import userService from "./utils/userService";
+
+export default function App() {
+  const [user, setUser] = useState(userService.getUser()); // if theres a token, grab it, if not the value will be null
+
+  // we need a function to pass down to LoginPage or the Signup page to be called after
+  // the api request to login or sign up has been made
+  function handleSignUpOrLogin() {
+    setUser(userService.getUser()); // getUser, gets the jwt from localstorage and decodes it
+  }
+
+  function handleLogout() {
+
+    console.log('being called')
+    userService.logout();
+    setUser(null);
+  }
+  if (user) {
+    // are we logged in?
+    return (
+      <Routes>
+        <Route
+          path="/login"
+          element={<LoginPage handleSignUpOrLogin={handleSignUpOrLogin} />}
+        />
+        <Route
+          path="/signup"
+          element={<SignUpPage handleSignUpOrLogin={handleSignUpOrLogin} />}
+        />
+        <Route
+          path="/:username"
+          element={
+            <ProfilePage loggedUser={user} handleLogout={handleLogout} />
+          }
+        />
+      </Routes>
+    );
+}
+
   return (
     <Routes>
-      <Route path="/" element={<h1>Home Pageeeeeeeeeee</h1>} />
-      <Route path="/login" element={<LoginPage />} />
+      <Route
+        path="/login"
+        element={<LoginPage handleSignUpOrLogin={handleSignUpOrLogin} />}
+      />
+      <Route
+        path="/signup"
+        element={<SignUpPage handleSignUpOrLogin={handleSignUpOrLogin} />}
+      />
+      <Route path="/*" element={<Navigate to="/login" />} />
     </Routes>
   );
 }
 
-export default App;
+
---
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,91 @@
+import { useState, useEffect } from "react";
+
+import PageHeader from "../../components/PageHeader/PageHeader";
+
+import { useNavigate } from "react-router-dom";
+
+
+
+
+import { useParams } from "react-router-dom";
+
+import { Grid } from "semantic-ui-react";
+
+import userService from "../../utils/userService";
+
+function ProfilePage({ loggedUser, handleLogout }) {
+    const [posts, setPosts] = useState([]);
+    const [profileUser, setProfileUser] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+  
+    const { username } = useParams(); 
+  
+    console.log("username in Profile page -> ", username);
+
+  
+    async function getProfile() {
+      try {
+        
+        const response = await userService.getProfile(username);
+  
+        setLoading(false); 
+        setPosts(response.data);
+        setProfileUser(response.user);
+        console.log(response, " <- data is getprofile");
+      } catch (err) {
+        console.log(
+          err.message,
+          " error in getProfile something went wrong with the getProfile api request, check server terminal"
+        );
+        setLoading(false);
+        setError("Profile does not exist"); 
+      }
+    }
+  
+    useEffect(() => {
+      getProfile();
+    }, [username]);
+  
+    if (error) {
+      return (
+        <>
+          <PageHeader handleLogout={handleLogout} loggedUser={loggedUser}/>
+          <ErrorMessage error={error} />;
+        </>
+      );
+    }
+  
+    if (loading) {
+      return (
+        <>
+          <PageHeader handleLogout={handleLogout} loggedUser={loggedUser}/>
+          <Loader />
+        </>
+      );
+    }
+  
+    return (
+      <Grid>
+        <Grid.Row>
+          <Grid.Column>
+            <PageHeader handleLogout={handleLogout} loggedUser={loggedUser}/>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row centered>
+          <Grid.Column style={{ maxWidth: 750 }}>
+            <PostDisplay
+              posts={posts}
+              numPhotosCol={4}
+              isProfile={true}
+              loading={loading}
+              loggedUser={loggedUser}
+            />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
+  }
+  
+  export default ProfilePage;
+  
